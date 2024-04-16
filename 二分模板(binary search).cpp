7393a186a9f59495395663360d9c70a34f160bf8
firezaizai <150493616+firezaizai@users.noreply.github.com>
2024-04-16 20:21:20 +0800

    Update 二分模板(binary search).cpp
    修改了int中'in'变成中文的问题
    （Modified the problem of 'in' becoming Chinese in int）

--- 二分模板(binary search).cpp
+++ 二分模板(binary search).cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-在t main()
+int main()
 {
-	在t a;
+	int a;
 	cin>>a;
-	在t l=1，r=1e9;
+	int l=1，r=1e9;
 	while(l<r)
 	{
-		在t mid=(l+r)/2;
+		在in mid=(l+r)/2;
 		if(mid<a) l=mid+1;
 		else r=mid;
 	}
